00f5b90ed98a7bcd8f35c2f7068841e4a9eac134
SimonsJace10 <Simons.Jace10@svvsd.org>
2021-12-08 21:55:12 -0700

    receipt writing function halfway done & everything before that

--- FRCC_C++_Final/Final.cpp
+++ FRCC_C++_Final/Final.cpp
@@ -15,7 +15,9 @@ Brief Description of this program: This is a program for a hypothetical coffee s
 
 // including io stream allows the program to access the flow of input and output through the system
 #include <iostream>
+// string allows string functions such as examining them char by char
 #include <string>
+// fstream allows access to both read and write of files
 #include <fstream>
 
 // using standard namespace allows us to omit 'std' in front of functions like cout and cin
@@ -23,14 +25,20 @@ using namespace std;
 
 // declare new functions
 int countLines(ifstream&);
-void openFile (ifstream&);
+void openFile (ifstream&, string);
 void displayMenu (ifstream&, string [], double []);
+void writeToReceipt (ifstream &menuFile, ofstream &receiptFile, string itemNames[], double itemPrices[]);
+
+// global vars
+const string menuPricesFile = "qbc_menu_prices.txt";
+const string receiptOutputFile = "qbc_sales_receipt.txt";
 
 // main serves as the driver code for this program. This is where we handle input from the user and call the display
 // menu and print receipt functions based on logic done in the main function
 int main() {
-    // create file object
+    // create file objects
     ifstream menuFile;
+    ofstream receiptFile;
 
     // create char to store input into
     char orderInquiry;
@@ -47,17 +55,27 @@ int main() {
         // and re-intake input
         cin >> orderInquiry;
     }
+    if (orderInquiry == 'n') {
+        cout << "No receipt required. Thanks!\n";
+        exit(1);
+    }
     // create parallel arrays to store the name of the item and the price of the item using the value from countLines
     string itemNames [countLines(menuFile)];
     double itemPrices [countLines(menuFile)];
-
+    // call the display menu function to call it the first time
     displayMenu(menuFile, itemNames, itemPrices);
+
+    // start an order
+    writeToReceipt(menuFile, receiptFile, itemNames, itemPrices);
+
+    // since main is required to return an integer value, returning zero effectively exits the program cleanly
+    return 0;
 }
 
 // this function counts the lines in the menu text file such that we can use that number to create arrays containing the items
 int countLines (ifstream& menuFile) {
     // open the file
-    openFile(menuFile);
+    openFile(menuFile, menuPricesFile);
     // create string to store current line in
     string currentLine;
     // create int to store number of lines
@@ -67,13 +85,14 @@ int countLines (ifstream& menuFile) {
         // increment the counter variable
         menuFileLines++;
     }
+    menuFile.close();
     return menuFileLines;
 }
 
-// openFile() simply opens a file so it can be passed into further functions
-void openFile(ifstream& file) {
+// openFile() simply opens an INPUT file so it can be passed into further functions
+void openFile(ifstream& file, string name) {
     // open the given file
-    file.open("qbc_menu_prices.txt");
+    file.open(name);
     // if it fails, report it as such
     if (file.fail()) {
         cout << "Failed to open file.\n";
@@ -92,15 +111,15 @@ void displayMenu (ifstream& menuFile, string itemNames[], double itemPrices[]) {
     string currentLine;
     // the index counter serves to count along the side of the menu (1. coffee 2. tea, etc.) and to assign the values to
     // the proper indexes of the parallel arrays
-    int indexCounter;
+    int indexCounter = 0;
     // open the passed in file
-    openFile(menuFile);
+    openFile(menuFile, menuPricesFile);
     // while we are still getting new lines ...
     while (getline (menuFile, currentLine)) {
         itemNames[indexCounter] = "";
-        itemPrices[indexCounter] = 0.0;
+        tempPrice = "";
         // ... start iterating through the characters in the currentLine
-        for (int index = 0; index < currentLine.size()-1; index++) {
+        for (int index = 0; index < currentLine.size(); index++) {
             // if it's a letter ...
             if (isalpha(currentLine[index]) && !isspace(currentLine[index])) {
                 // ... concatenate the letter to the name of the item
@@ -117,14 +136,72 @@ void displayMenu (ifstream& menuFile, string itemNames[], double itemPrices[]) {
                 continue;
             }
         }
-        itemPrices[indexCounter]= stod((tempPrice));
+        itemPrices[indexCounter] = (stod((tempPrice)));
         // after every currentLine is processed, print out the currentLine with the appropriate item number, name and price,
         // formatting with the \t (tab stop) character.
-        cout << "| " << indexCounter << ". " << itemNames[indexCounter] << " \t $" << itemPrices[indexCounter] << " |\n";
+        cout << "| " << indexCounter+1 << ". " << itemNames[indexCounter] << "\t $" << itemPrices[indexCounter] << " |\n";
         // increment the counter for the parallel arrays and length of the menu
         indexCounter++;
     }
     // display the bottom line of the menu board
     cout << "------------------------\n";
     menuFile.close();
+}
+
+void writeToReceipt (ifstream& menuFile, ofstream& receiptFile, string itemNames[], double itemPrices[]) {
+
+    // the lines integer is how long the menu is, because it is referenced several times in this file for formatting
+    const int lines = countLines(menuFile);
+
+    // create string to store selection inquiry
+    int selectionInquiry;
+
+    // create a boolean to control the looping of the order tabulation
+    char continueOrdering;
+
+    // open the receipt file because the open file function only opens input files, since we only open the output
+    // function to write the receipt and to read it(or create one)
+    receiptFile.open(receiptOutputFile);
+
+    // start writing the header to the file
+    receiptFile << "\tJACE'S COFFEE SHOP\n";
+    receiptFile << "\tSALES RECEIPT\n";
+    receiptFile << "Owner: Jace Simons\n 123-456-7890";
+    receiptFile << "------------------------\n";
+
+    // create receipt vars
+    double subtotal;
+    double tax;
+
+    while (tolower(continueOrdering) == 'y') {
+        // prompt user
+        cout << "Please make a selection ( 1 - " << lines << " ): \n";
+        // store selection
+        cin >> selectionInquiry;
+
+        while (selectionInquiry <= 0 || selectionInquiry >= lines) {
+            cout << "Invalid selection; please input an integer 1 - " << lines << endl;
+            cin.clear();
+            cin >> selectionInquiry;
+        }
+
+        // based on the selection, start tabulating the order
+        int quantityInquiry;
+        cout << "How many " << itemNames[selectionInquiry-1] << " (s) do you want?\n";
+        cin >> quantityInquiry;
+
+        while (quantityInquiry < 0 || quantityInquiry > lines) {
+            cout << "Invalid quantity. Please input an integer greater than 0.\n";
+            cin.clear();
+            cin >> quantityInquiry;
+        }
+
+        cout << quantityInquiry << "\t" << itemNames[selectionInquiry-1] << "\t" << itemPrices[selectionInquiry-1] * quantityInquiry << endl;
+
+        cout << "Anything else I can get for you ( y/n )?\n";
+        cin >> continueOrdering;
+    }
+    receiptFile << "------------------------\n";
+    receiptFile << "Subtotal: " <<
+
 }
