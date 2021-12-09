bfb5f6319386481e31613d0bac68d8d56fd932c7
SimonsJace10 <Simons.Jace10@svvsd.org>
2021-12-08 18:06:41 -0700

    input validation for menu items working with .find

--- FRCC_C++_Final/Final.cpp
+++ FRCC_C++_Final/Final.cpp
@@ -21,21 +21,24 @@ Brief Description of this program: This is a program for a hypothetical coffee s
 // using standard namespace allows us to omit 'std' in front of functions like cout and cin
 using namespace std;
 
-// declare and define menu item class
-//class menuItem {
-//public:
-//    string name;
-//    double price;
-//};
-
 // declare new functions
-void displayMenu ();
-void tab ();
+void displayMenu (ifstream&);
+void openFile (ifstream&);
+void tab (int menuLength, ifstream&);
 void salesReceipt ();
 
 // main serves as the driver code for this program. This is where we handle input from the user and call the display
 // menu and print receipt functions based on logic done in the main function
 int main() {
+//    int strIndex;
+//    int pos = 0;
+//    string str1 = "test haha";
+//    string str2 = "test";
+//    while (strIndex = str1.find(str2, pos) != string::npos) {
+//        cout << "found at " << strIndex;
+//        pos = strIndex + 1;
+//    }
+
     // create vars
     char placeOrderInquiry;
 
@@ -61,28 +64,36 @@ int main() {
         // exit program
         exit(1);
     }
+
+    // open file to pass to functions
+    // create file object
+    ifstream menuFile;
+    openFile(menuFile);
+
     // display the menu in this case, because if the user said no then the program has already exited
-    displayMenu();
+    displayMenu(menuFile);
 
     // return 0 exits the program
     return 0;
 }
 
-void displayMenu () {
-    // The next block handles input from file 'qbc_menu_prices.txt'
-    // variable to store the menu items
-    int menuLength = 1;
-    // declare string variable to relay lines from file
-    string line;
-    // create object to handle the file
-    ifstream menuFile;
-    // open the file
-    menuFile.open("qbc_menu_prices.txt");
+// openFile() simply opens a file so it can be passed into further functions
+void openFile(ifstream& file) {
+    // open the given file
+    file.open("qbc_menu_prices.txt");
     // if it fails, report it as such
-    if (menuFile.fail()) {
+    if (file.fail()) {
         cout << "Failed to open file.\n";
         exit (1);
     }
+}
+
+void displayMenu (ifstream& menuFile) {
+    // The next block handles input from file 'qbc_menu_prices.txt'
+    // variable to store the number of menu items
+    int menuLength = 1;
+    // declare string variable to relay lines from file
+    string line;
 
     // creating strings for the strings taken in from the file
     string itemName;
@@ -121,33 +132,55 @@ void displayMenu () {
     }
     // display the last line of the menu
     cout << "------------------------\n";
-    //tab(menuLength);
+    menuFile.close();
+    tab(menuLength, menuFile);
 }
 
-void tab () {
+void tab (int menuLength, ifstream& menuFile) {
     // create inquiry var to store selection
-    int selectionInquiry;
+    string selectionInquiry;
     // prompt user
-    cout << "Please make a selection.\n";
+    cout << "Please make a selection [ type the name of the item ]\n";
     // take in and store input
     cin >> selectionInquiry;
 
-    // input validation
-    // while the input is less than 0 or more than menu length ...
-    while (selectionInquiry <= 0 || selectionInquiry > 5) {
-        // ... tell the user ...
-        cout << "Please input a valid selection [ 1 - " << 5 << " ]\n";
-        // ... clear the input buffer
-        cin.clear();
-        // re-intake input
-        cin >> selectionInquiry;
+    string line;
+    string itemName;
+    string itemPrice;
+    double price;
+    int strIndex;
+    int pos = 0;
+    cout << selectionInquiry;
+    openFile(menuFile);
+    while (getline(menuFile, line)) {
+        // ...
+        itemName = "", itemPrice = "";
+        cout << line;
+        while (strIndex = line.find(selectionInquiry, pos) != string::npos) {
+            itemName += selectionInquiry;
+            cout << "found at " << strIndex;
+            pos = strIndex + 1;
+            for (int index = 0; index < line.length(); index++) {
+                if (isdigit(line[index]) || line[index] == '.') {
+                    itemPrice += line[index];
+                }
+                else {
+                    continue;
+            }
+        }
     }
-    // begin order placement
-    int itemCount;
-    cout << "How many would you like?\n";
 
+    price = stod(itemPrice);
+
+        // begin order placement
+        double itemQuantityInquiry = 0;
+        cout << "How many " << itemName << " would you like?\n";
+        cin >> itemQuantityInquiry;
+
+        cout << itemQuantityInquiry << " of " << itemName << " added to your order for " << price * itemQuantityInquiry;
+    }
 }
 
-void salesReceipt() {
+void displaySalesReceipt() {
     // this function handles printing a receipt based on the order from the user
 }
