12df1ffc47306e3e3050ea2b7858fee7bbd49b5c
SimonsJace10 <Simons.Jace10@svvsd.org>
2021-12-08 20:28:29 -0700

    restart

--- FRCC_C++_Final/Final.cpp
+++ FRCC_C++_Final/Final.cpp
@@ -22,59 +22,52 @@ Brief Description of this program: This is a program for a hypothetical coffee s
 using namespace std;
 
 // declare new functions
-void displayMenu (ifstream&);
+int countLines(ifstream&);
 void openFile (ifstream&);
-void tab (int menuLength, ifstream&);
-void salesReceipt ();
+void displayMenu (ifstream&, string [], double []);
 
 // main serves as the driver code for this program. This is where we handle input from the user and call the display
 // menu and print receipt functions based on logic done in the main function
 int main() {
-//    int strIndex;
-//    int pos = 0;
-//    string str1 = "test haha";
-//    string str2 = "test";
-//    while (strIndex = str1.find(str2, pos) != string::npos) {
-//        cout << "found at " << strIndex;
-//        pos = strIndex + 1;
-//    }
-
-    // create vars
-    char placeOrderInquiry;
-
-    // prompt user
-    cout << "Would you like to place and order? [y or n]\n";
-    // take input and store in placeOrderInquiry
-    cin >> placeOrderInquiry;
-
-    // input validation
-    // while the input isn't y or n ...
-    while (tolower(placeOrderInquiry) != 'y' && tolower(placeOrderInquiry) != 'n') {
-        // ... tell the user to enter a valid input ...
-        cout << "Invalid input, please input either y or n.\n";
-        // ... clear the input buffer
-        cin.clear();
-        // re-take in the input
-        cin >> placeOrderInquiry;
-    }
-    // if the input is no, exit the program
-    if (placeOrderInquiry == 'n') {
-        // say goodbye
-        cout << "No receipt required. Thanks!";
-        // exit program
-        exit(1);
-    }
-
-    // open file to pass to functions
     // create file object
     ifstream menuFile;
+
+    // create char to store input into
+    char orderInquiry;
+    // greet user
+    cout << "Welcome to Jace's Coffee Shop! Would you like to place an order? (y/n)\n";
+    // store input
+    cin >> orderInquiry;
+    // validate input for starting an order. While the input isn't y and it isn't n ...
+    while (tolower(orderInquiry) != 'y' && tolower(orderInquiry) != 'n') {
+        // ... tell user,
+        cout << "Invalid input- please input y or n.\n";
+        // clear the input buffer,
+        cin.clear();
+        // and re-intake input
+        cin >> orderInquiry;
+    }
+    // create parallel arrays to store the name of the item and the price of the item using the value from countLines
+    string itemNames [countLines(menuFile)];
+    double itemPrices [countLines(menuFile)];
+
+    displayMenu(menuFile, itemNames, itemPrices);
+}
+
+// this function counts the lines in the menu text file such that we can use that number to create arrays containing the items
+int countLines (ifstream& menuFile) {
+    // open the file
     openFile(menuFile);
-
-    // display the menu in this case, because if the user said no then the program has already exited
-    displayMenu(menuFile);
-
-    // return 0 exits the program
-    return 0;
+    // create string to store current line in
+    string currentLine;
+    // create int to store number of lines
+    int menuFileLines = 0;
+    // while we continue to get a next line ...
+    while (getline(menuFile, currentLine)) {
+        // increment the counter variable
+        menuFileLines++;
+    }
+    return menuFileLines;
 }
 
 // openFile() simply opens a file so it can be passed into further functions
@@ -88,99 +81,50 @@ void openFile(ifstream& file) {
     }
 }
 
-void displayMenu (ifstream& menuFile) {
-    // The next block handles input from file 'qbc_menu_prices.txt'
-    // variable to store the number of menu items
-    int menuLength = 1;
-    // declare string variable to relay lines from file
-    string line;
-
-    // creating strings for the strings taken in from the file
-    string itemName;
-    string itemPrice;
+void displayMenu (ifstream& menuFile, string itemNames[], double itemPrices[]) {
+    // populate the arrays
     // begin printing out the menu, starting with the header
     cout << "       ~~ MENU ~~\n";
     cout << "------------------------\n";
 
-
+    // create temporary name and price variables to store the name and price extracted from a given line of the menu file
+    string tempPrice;
+    string currentLine;
+    // the index counter serves to count along the side of the menu (1. coffee 2. tea, etc.) and to assign the values to
+    // the proper indexes of the parallel arrays
+    int indexCounter;
+    // open the passed in file
+    openFile(menuFile);
     // while we are still getting new lines ...
-    while (getline (menuFile, line)) {
-        // ...
-        itemName = "", itemPrice = "";
-        for (int index = 0; index < line.length(); index++) {
+    while (getline (menuFile, currentLine)) {
+        itemNames[indexCounter] = "";
+        itemPrices[indexCounter] = 0.0;
+        // ... start iterating through the characters in the currentLine
+        for (int index = 0; index < currentLine.size()-1; index++) {
             // if it's a letter ...
-            if (isalpha(line[index]) && !isspace(line[index])) {
+            if (isalpha(currentLine[index]) && !isspace(currentLine[index])) {
                 // ... concatenate the letter to the name of the item
-                itemName += line[index];
+                itemNames[indexCounter] += currentLine[index];
             }
-            // if it's a digit or a period ...
-            else if (isdigit(line[index]) || line[index] == '.') {
+                // if it's a digit or a period ...
+            else if (isdigit(currentLine[index]) || currentLine[index] == '.') {
                 // concatenate it to the string price of the item
-                itemPrice += line[index];
+                tempPrice += currentLine[index];
             }
-            // if it's a space or newline ...
-            else if (isspace(line[index]) || line[index] == '\n') {
+                // if it's a space or newline ...
+            else if (isspace(currentLine[index]) || currentLine[index] == '\n') {
                 // iterate
                 continue;
             }
         }
-        // after every line is processed, print out the line with the appropriate item number, name and price,
+        itemPrices[indexCounter]= stod((tempPrice));
+        // after every currentLine is processed, print out the currentLine with the appropriate item number, name and price,
         // formatting with the \t (tab stop) character.
-        cout << "| " << menuLength << ". " << itemName << " \t $" << itemPrice << " |\n";
-        // increment the length of the menu
-        menuLength++;
+        cout << "| " << indexCounter << ". " << itemNames[indexCounter] << " \t $" << itemPrices[indexCounter] << " |\n";
+        // increment the counter for the parallel arrays and length of the menu
+        indexCounter++;
     }
-    // display the last line of the menu
+    // display the bottom line of the menu board
     cout << "------------------------\n";
     menuFile.close();
-    tab(menuLength, menuFile);
-}
-
-void tab (int menuLength, ifstream& menuFile) {
-    // create inquiry var to store selection
-    string selectionInquiry;
-    // prompt user
-    cout << "Please make a selection [ type the name of the item ]\n";
-    // take in and store input
-    cin >> selectionInquiry;
-
-    string line;
-    string itemName;
-    string itemPrice;
-    double price;
-    int strIndex;
-    int pos = 0;
-    cout << selectionInquiry;
-    openFile(menuFile);
-    while (getline(menuFile, line)) {
-        // ...
-        itemName = "", itemPrice = "";
-        cout << line;
-        while (strIndex = line.find(selectionInquiry, pos) != string::npos) {
-            itemName += selectionInquiry;
-            cout << "found at " << strIndex;
-            pos = strIndex + 1;
-            for (int index = 0; index < line.length(); index++) {
-                if (isdigit(line[index]) || line[index] == '.') {
-                    itemPrice += line[index];
-                }
-                else {
-                    continue;
-            }
-        }
-    }
-
-    price = stod(itemPrice);
-
-        // begin order placement
-        double itemQuantityInquiry = 0;
-        cout << "How many " << itemName << " would you like?\n";
-        cin >> itemQuantityInquiry;
-
-        cout << itemQuantityInquiry << " of " << itemName << " added to your order for " << price * itemQuantityInquiry;
-    }
-}
-
-void displaySalesReceipt() {
-    // this function handles printing a receipt based on the order from the user
 }
